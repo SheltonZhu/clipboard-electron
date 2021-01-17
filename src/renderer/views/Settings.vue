abd682ab59475efd861ee7ca3da70fd59c785f65
SheltonZhu <498220739@qq.com>
2021-01-17 21:02:49 +0800

    add some color theme

--- src/renderer/views/Settings.vue
+++ src/renderer/views/Settings.vue
@@ -370,10 +370,13 @@ export default {
       bgList: [],
       bgColor: "rgba(255, 255, 255, 0.72)",
       predefineColors: [
-        "rgba(255, 255, 255, 0.72)",
-        "rgba(255,179,167, 0.72)",
+        "rgba(255, 255, 255, 0.72)", //原版
+        "rgba(255, 230, 230, 0.68)", //紫粉套
+        "rgba(144, 240, 144, 0.5)", //蓝黄套
+        "rgba(157, 1, 145, 0.27)", //粉黄蓝
+        "rgba(208, 232, 242, 0.52)", //灰蓝
+        "rgba(111, 74, 142, 0.71)", //黑紫
         "rgba(255, 69, 0, 0.68)",
-        "rgba(144, 240, 144, 0.5)",
         "hsla(209, 100%, 56%, 0.73)",
         "rgba(35, 37, 35, 0.71)",
         "rgb(255, 120, 0)",
@@ -382,18 +385,35 @@ export default {
       ],
       labelFontColor: "rgba(44, 62, 80, 1)",
       predefineLabelFontColors: [
-        "#2c3e50",
-        "#fff",
-        "#000",
+        "rgba(44, 62, 80, 1)", //原版
+        "rgba(97, 85, 166, 1)", //紫粉套
+        "rgba(21, 62, 144, 1)", //蓝黄套
+        "rgba(18, 0, 120, 1)", //粉黄蓝
+        "rgba(69, 98, 104, 1)", //灰蓝
+        "rgba(5, 5, 5, 1)", //黑紫
         "rgba(99, 145, 230, 1)",
         "rgba(249, 252, 44, 1)",
         "rgba(239, 38, 85, 1)",
         "rgba(202, 38, 239, 1)"
       ],
       labelFontColorSelect: "rgba(255, 255, 255, 1)",
-      predefineLabelFontColorsSelect: ["rgba(255, 255, 255, 1)"],
+      predefineLabelFontColorsSelect: [
+        "rgba(255, 255, 255, 1)", //原版
+        "rgba(255, 171, 225, 1)", //紫粉套
+        "rgba(255, 250, 164, 1)", //蓝黄套
+        "rgba(254, 205, 26, 1)", //粉黄蓝
+        "rgba(252, 248, 236, 1)", //灰蓝
+        "rgba(235, 235, 235, 1)" //黑紫
+      ],
       labelBgColorSelect: "rgba(185,185,185,0.82)",
-      predefineLabelBgColorsSelect: ["rgba(185, 185, 185, 0.82)"],
+      predefineLabelBgColorsSelect: [
+        "rgba(185, 185, 185, 0.82)", //原版
+        "rgba(166, 133, 226, 1)", //紫粉套
+        "rgba(14, 73, 181, 1)", //蓝黄套
+        "rgba(253, 58, 105, 1)", //粉黄蓝
+        "rgba(121, 163, 177, 1)", //灰蓝
+        "rgba(34, 31, 59, 1)" //黑紫
+      ],
       autoBoot: false,
       directPaste: true,
       hideWhenBlur: false,
